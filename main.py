249e031846e366487ae7ea8b33e495b302135d61
test4ment <nkaliczkij@yandex.ru>
2024-02-21 23:53:32 +0600

    demo window added; path window added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from models import *
-from windows import FitPredict, AboutWindow
+from windows import FitPredict, AboutWindow, DemoWindow
 
 class MainWindow(tk.Tk):
   def __init__(self):
@@ -15,6 +15,9 @@ class MainWindow(tk.Tk):
     
     def about():
       AboutWindow()
+      
+    def demo():
+      DemoWindow()
     
     super().__init__()
     
@@ -22,7 +25,7 @@ class MainWindow(tk.Tk):
     self.geometry("400x400")
 
 
-    self.mlTaskFrame = ttk.Frame(borderwidth = 2)
+    self.mlTaskFrame = ttk.Frame(borderwidth = 0)
     
     self.selectTaskText = ttk.Label(self.mlTaskFrame, text = "Решаемая задача:")
     self.mlTask = ttk.Combobox(self.mlTaskFrame, state = "readonly", values = list(Tasks))
@@ -42,8 +45,17 @@ class MainWindow(tk.Tk):
     self.modelWorkerButton = ttk.Button(self, text = "Начать работу", command = modelWindowOpener)
     self.modelWorkerButton.pack(anchor = "nw", padx = 12, pady = 5, ipadx = 20)
 
-    self.aboutButton = ttk.Button(self, text = "About", command = about)
-    self.aboutButton.pack(anchor = "sw", expand = True, padx = 12, pady = 12)
+    self.aboutDemoFrame = ttk.Frame(borderwidth = 2)
+    self.aboutButton = ttk.Button(self.aboutDemoFrame, text = "About", command = about)
+    self.demoButton = ttk.Button(self.aboutDemoFrame, text = "Demo", command = demo)
+    
+    self.aboutButton.grid(row = 0, column = 0, padx = 12)
+    self.demoButton.grid(row = 0, column = 1)
+    
+    self.aboutDemoFrame.pack(anchor = "sw", expand = True, padx = 12, pady = 12)
+
+    self.protocol("WM_DELETE_WINDOW", self.destroy)
+
   
 
 if __name__ == '__main__':
--- windows.py
+++ windows.py
@@ -1,14 +1,63 @@
 import tkinter as tk
 from tkinter import ttk
+# import pandas as pd
+from pandas import read_csv
 from models import *
 
-class AboutWindow(tk.Tk):
+class AboutWindow(tk.Toplevel):
     def __init__(self):
         super().__init__()
         # write something bout yourself
+
+class PathWindow(tk.Toplevel):
+    def __init__(self):
+        super().__init__()
+        
+        def confirm():
+            try:
+                path = self.textEntry.get()
+                read_csv(path)
+                self.destroy()
+            except Exception as e:
+                self.textEntry.delete(0, tk.END)
+                self.textEntry.insert(0, e)
+            
+        self.title("Select .csv file...")
+        self.geometry("300x50")
+
+        self.frame = ttk.Frame(self)
+        
+        self.textEntry = ttk.Entry(self.frame)
+        self.textEntry.pack(side = "left", padx = 12, pady = 12, fill = "x", expand = True)
+        self.confirmButton = ttk.Button(self.frame, text = "Confirm", command = confirm)
+        self.confirmButton.pack(side = "left", fill = "x", padx = 6)
+        
+        self.frame.pack(expand = True, fill = "x")
+        
+        self.update()
+        self.minsize(self.winfo_width(), self.winfo_height())
+
+        
+    
+class DemoWindow(tk.Toplevel):
+    def __init__(self):
+        super().__init__()
+        
+        def load_data():
+            PathWindow()
+            
+                
+        self.loadedText = "Load data"
+        self.loadData = ttk.Button(self, text = self.loadedText, command = load_data)
+        
+        self.loadData.pack(anchor = "nw")
+        
+        # from sklearn.linear_model import SGDClassifier
+        
+        # clsf = SGDClassifier()
         
 
-class FitPredict(tk.Tk):
+class FitPredict(tk.Toplevel):
     def __init__(self, model: str):
         super().__init__()
         
@@ -28,6 +77,7 @@ class FitPredict(tk.Tk):
         self.predictButton = ttk.Button(self.fitpredictFrame, text = "Predict")
         self.fitButton.grid(row = 0, column = 0)
         self.predictButton.grid(row = 0, column = 1)
+        self.statusText = ttk.Label(text = "status")
         
         self.fitpredictFrame.pack(anchor = "nw", padx = 12, pady = 12)
 
